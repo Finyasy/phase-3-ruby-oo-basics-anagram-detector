328a92bf8dbb6d7946eba83ce406547b1745ffa7
Bryan Bosire <bryanbosire@gmail.com>
2022-08-18 15:45:06 +0300

    commit tree fe473633decf63d9ea1716baadc674cf7611e8a3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    # .new has an initialize method that takes in a word
+    def initialize(word)
+        @word = word
+    end
+
+    # .match takes in an array of words and returns an array of anagrams
+    def match(array)
+        array.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
